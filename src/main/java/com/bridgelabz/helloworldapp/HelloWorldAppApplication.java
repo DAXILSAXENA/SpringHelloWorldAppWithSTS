c857f1b68f89ba601e6f78710cd45e0468047830
DAXILSAXENA <daxildaxil@gmail.com>
2021-08-08 13:25:43 +0530

    [Daxil] Added:Hello World App With STS

---
+++ src/main/java/com/bridgelabz/helloworldapp/HelloWorldAppApplication.java
@@ -0,0 +1,19 @@
+/**
+ * Purpose : Create a Helloworld Spring App
+ * 
+ * @author DAXIL SAXENA
+ * @since 08.08.2021
+ */
+package com.bridgelabz.helloworldapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAppApplication.class, args);
+	}
+
+}
